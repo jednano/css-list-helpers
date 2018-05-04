981bb5704f6bebed17aa3e76a1df7cb94f83dbd2
Jed Mao <jedmao@outlook.com>
2018-05-03 17:06:01 -0700

    convert code to TypeScript

--- index.ts
+++ index.ts
@@ -1,82 +1,75 @@
-var t = require('tcomb');
+export interface IOptions {
+	last?: boolean;
+}
 
-var Options = t.struct({
-	last: t.maybe(t.Boolean)
-});
+export function split(
+	value: string,
+	separators: string[],
+	options: IOptions = {},
+) {
+	return _split(value, separators, options || {});
+}
 
-var helpers = {
+export function splitBySpaces(value: string) {
+	const spaces = [' ', '\n', '\t'];
+	return split(value, spaces);
+}
 
-	split: function(value, separators, options) {
-		return split(value, separators, options || {});
-	},
+export function splitByCommas(value: string) {
+	const comma = ',';
+	return split(value, [comma], { last: true });
+}
 
-	splitBySpaces: t.func(t.String, t.Array).of(
-		function(value) {
-			var spaces = [' ', '\n', '\t'];
-			return helpers.split(value, spaces);
-		}
-	),
+export function _split(
+	value: string,
+	separators: string[],
+	options: IOptions = {},
+) {
+	const array   = [];
+	let current = '';
+	let splitMe   = false;
 
-	splitByCommas: t.func(t.String, t.Array).of(
-		function(value) {
-			var comma = ',';
-			return helpers.split(value, [comma], { last: true });
-		}
-	)
+	let func    = 0;
+	let quote: '"' | '\'' | false = false;
+	let escape  = false;
 
-};
+	for (const char of value) {
 
-var split = t.func([t.String, t.Array, Options], t.Array).of(
-	function(value, separators, options) {
-		var array   = [];
-		var current = '';
-		var split   = false;
-
-		var func    = 0;
-		var quote   = false;
-		var escape  = false;
-
-		for (var i = 0; i < value.length; i++) {
-			var char = value[i];
-
-			if (quote) {
-				if (escape) {
-					escape = false;
-				} else if (char === '\\') {
-					escape = true;
-				} else if (char === quote) {
-					quote = false;
-				}
-			} else if (char === '"' || char === '\'') {
-				quote = char;
-			} else if (char === '(') {
-				func += 1;
-			} else if (char === ')') {
-				if (func > 0) {
-					func -= 1;
-				}
-			} else if (func === 0) {
-				if (separators.indexOf(char) !== -1) {
-					split = true;
-				}
+		if (quote) {
+			if (escape) {
+				escape = false;
+			} else if (char === '\\') {
+				escape = true;
+			} else if (char === quote) {
+				quote = false;
 			}
-
-			if (split) {
-				if (current !== '') {
-					array.push(current.trim());
-				}
-				current = '';
-				split = false;
-			} else {
-				current += char;
+		} else if (char === '"' || char === '\'') {
+			quote = char;
+		} else if (char === '(') {
+			func += 1;
+		} else if (char === ')') {
+			if (func > 0) {
+				func -= 1;
+			}
+		} else if (func === 0) {
+			if (separators.indexOf(char) !== -1) {
+				splitMe = true;
 			}
 		}
 
-		if (options.last || current !== '') {
-			array.push(current.trim());
+		if (split) {
+			if (current !== '') {
+				array.push(current.trim());
+			}
+			current = '';
+			splitMe = false;
+		} else {
+			current += char;
 		}
-		return array;
 	}
-);
 
-module.exports = helpers;
+	if (options.last || current !== '') {
+		array.push(current.trim());
+	}
+	return array;
+}
